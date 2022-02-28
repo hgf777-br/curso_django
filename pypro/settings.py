850f77335a2584c02740a88f97c3df506adcaa51
hgf777-br <hgf777@gmail.com>
2022-02-28 12:55:39 -0300

    collect fast

--- pypro/settings.py
+++ pypro/settings.py
@@ -144,7 +144,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
     AWS_DEFAULT_ACL = 'private'
-    
+
     COLLECTFAST_ENABLE = True
 
     # Static Assets
