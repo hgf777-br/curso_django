989b2ff781eb5b188c6fcf482ac740d6466e6a18
hgf777-br <hgf777@gmail.com>
2022-03-16 09:47:57 -0300

    video

---
+++ pypro/aperitivos/test_aperitivo.py
@@ -0,0 +1,15 @@
+import pytest
+import pytest
+from django.urls import reverse
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
