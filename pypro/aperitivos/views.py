e2379d9d18040a4cc60c5fad37a685dad743fff6
hgf777-br <hgf777@gmail.com>
2022-03-21 20:50:58 -0300

    new admin page
    close #58

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,29 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id) -> None:
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
-
-videos = [
-    Video('motivacao', 'Motivação', 688602515),
-    Video('instalacao-windows', 'Instalação no Windows', 689777418)
-]
-
-videos_dict = {video.slug: video for video in videos}
+from django.db import models
+from pypro.aperitivos.models import Video
 
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    video = videos_dict[slug]
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
