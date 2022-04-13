b96b408148706d8e0e564658f07315765f241e6b
hgf777-br <hgf777@gmail.com>
2022-04-13 17:17:27 -0300

    consertando flake8
    close #72

--- pypro/modulos/context_processors.py
+++ pypro/modulos/context_processors.py
@@ -1,4 +1,5 @@
 from pypro.modulos import facade
 
+
 def listar_modulos(request):
     return {'MODULOS': facade.listar_modulos_ordenados()}
--- pypro/modulos/migrations/0003_populate_slugs.py
+++ pypro/modulos/migrations/0003_populate_slugs.py
@@ -3,11 +3,13 @@
 from django.db import migrations
 from django.utils.text import slugify
 
+
 def popular_slugs(apps, schema_editor):
     Modulo = apps.get_model('modulos', 'Modulo')
     for modulo in Modulo.objects.all():
         modulo.slug = slugify(modulo.titulo)
         modulo.save()
+
 
 class Migration(migrations.Migration):
 
